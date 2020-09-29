fe71f8deb53500fd98f9b361f3e6471357796bbf
Elena Vonder Meulen <evonder13@gmail.com>
2020-09-29 12:33:14 -0400

    More if statements

--- BankingProject/Account.cs
+++ BankingProject/Account.cs
@@ -13,12 +13,27 @@ namespace BankingProject
 
         public double Deposit (double amount)
         {
+            if (amount <= 0) 
+            {
+                Console.WriteLine($"Amount must be GT 0");
+                return Balance;
+            }
             Balance = Balance + amount;
             return Balance;
         }
 
         public double Withdraw(double amount)
         {
+            if(amount <= 0)//don't put negative number, will change to positive
+            {
+                Console.WriteLine($"Cannot withdraw negative amount");
+                return Balance;
+            }
+            if(Balance < amount)
+            {
+                Console.WriteLine($"Insufficient Funds!");
+                return Balance;
+            }
             //same as Balance = Balance - amount;
             Balance -= amount; 
             return Balance;
--- BankingProject/Program.cs
+++ BankingProject/Program.cs
@@ -10,6 +10,14 @@ namespace BankingProject
             acct1.print();
             var acct2 = new Account("MyChecking");
             acct2.print();
+            acct2.Deposit(1000);
+            acct2.print();
+            acct2.Withdraw(50);
+            acct2.print();
+            acct2.Deposit(-200);
+            acct2.print();
+            acct2.Withdraw(-1);
+            acct2.print();
         }
     }
 }
